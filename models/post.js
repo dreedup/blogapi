7a4da20ed147face1903e316a86faf1891d5f082
dreedup <andredransom@gmail.com>
2023-07-02 16:36:05 -0400

    completed models post

--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ const { model, Schema } = require('mongoose')
 const postSchema = new Schema ({
     title: {type: String, required: true},
     body: {type: String, required: true},
-    user: {type: Schema.Types.ObjectId, required: true, ref: 'User'}
+    user: {type: Schema.Types.ObjectId, required: true, ref: 'User'} //allows you to show which user the post belongs to
 })
 
 const Post = model('Post', postSchema)
